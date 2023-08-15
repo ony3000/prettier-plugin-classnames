c179bee170f60d028a0398dc4f592d97ac2968a1
Hyeonjong <nianelo4@gmail.com>
2023-08-16 01:05:38 +0900

    test: add embedded expression class name tests

--- tests/typescript/react-component.test.ts
+++ tests/typescript/react-component.test.ts
@@ -123,6 +123,99 @@ export default function Callout({ children }) {
 }
 `,
   },
+  {
+    name: 'string literal as embedded expression #1 (short enough)',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className={'rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4'}>
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className={
+        "rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4"
+      }
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'string literal as embedded expression #2',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className={'rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50'}>
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className={
+        "rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50"
+      }
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'template literal as embedded expression #1 (short enough)',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className={\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4\`}>
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className={\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4\`}
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'template literal as embedded expression #2',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className={\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50\`}>
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className={\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+        dark:border-neutral-500/30 dark:bg-neutral-900/50\`}
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
 ];
 
 describe('typescript/react-component', () => {
