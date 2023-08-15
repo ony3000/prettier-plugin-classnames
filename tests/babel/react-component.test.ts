02c46d8e6731945e119ecee5e47b7b367553251a
Hyeonjong <nianelo4@gmail.com>
2023-08-15 23:48:15 +0900

    test: add string literal class name tests

--- tests/babel/react-component.test.ts
+++ tests/babel/react-component.test.ts
@@ -8,9 +8,120 @@ const options = {
 
 const fixtures: Fixture[] = [
   {
-    name: '',
-    input: ``,
-    output: ``,
+    name: 'className attribute only #1 (short enough)',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4">
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4">
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'className attribute only #2',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50">
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+        dark:border-neutral-500/30 dark:bg-neutral-900/50"
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'className attribute only #3 (self closing tag)',
+    input: `
+export function Callout() {
+  return (
+    <div className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50" />
+  );
+}
+`,
+    output: `export function Callout() {
+  return (
+    <div
+      className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+        dark:border-neutral-500/30 dark:bg-neutral-900/50"
+    />
+  );
+}
+`,
+  },
+  {
+    name: 'along with other attributes (className is the first)',
+    input: `
+export default function Callout({ children }) {
+  return (
+    <div className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50" onFocus={() => {}} onMouseOver={() => {}} tabIndex={-1} title="Callout">
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export default function Callout({ children }) {
+  return (
+    <div
+      className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+        dark:border-neutral-500/30 dark:bg-neutral-900/50"
+      onFocus={() => {}}
+      onMouseOver={() => {}}
+      tabIndex={-1}
+      title="Callout"
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'along with other attributes (className is the last)',
+    input: `
+export default function Callout({ children }) {
+  return (
+    <div onFocus={() => {}} onMouseOver={() => {}} tabIndex={-1} title="Callout" className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50">
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export default function Callout({ children }) {
+  return (
+    <div
+      onFocus={() => {}}
+      onMouseOver={() => {}}
+      tabIndex={-1}
+      title="Callout"
+      className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+        dark:border-neutral-500/30 dark:bg-neutral-900/50"
+    >
+      {children}
+    </div>
+  );
+}
+`,
   },
 ];
 
