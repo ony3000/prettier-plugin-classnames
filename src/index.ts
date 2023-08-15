5b27c2df3f39615ff0415bbc214534a519502f63
Hyeonjong <nianelo4@gmail.com>
2023-08-15 16:53:11 +0900

    feat: define plugin printers
    The basic structure was copied from https://github.com/ony3000/prettier-plugin-brace-style/blob/v0.2.0/src/printers.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import type { Plugin } from 'prettier';
 
 import { parsers } from './parsers';
+import { printers } from './printers';
 
 const classnamesPlugin: Plugin = {
   parsers,
+  printers,
 };
 
 export default classnamesPlugin;
---
+++ src/printers.ts
@@ -0,0 +1,45 @@
+import type { AstPath, ParserOptions, Doc, Printer, Plugin } from 'prettier';
+import { format } from 'prettier';
+
+function createPrinter(parserName: 'babel' | 'typescript'): Printer {
+  function main(
+    path: AstPath,
+    options: ParserOptions,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    print: (path: AstPath) => Doc,
+  ): Doc {
+    const plugins = options.plugins.filter((plugin) => typeof plugin !== 'string') as Plugin[];
+    const pluginCandidate = plugins.find((plugin) => plugin.parsers?.[parserName]);
+
+    if (!pluginCandidate) {
+      throw new Error('A plugin with the given parser does not exist.');
+    }
+
+    const node = path.getValue();
+
+    if (node?.comments) {
+      node.comments.forEach((comment: any) => {
+        // eslint-disable-next-line no-param-reassign
+        comment.printed = true;
+      });
+    }
+
+    const { originalText } = options;
+    const formattedText = format(originalText, {
+      ...options,
+      plugins: [pluginCandidate],
+      endOfLine: 'lf',
+    });
+
+    return formattedText;
+  }
+
+  return {
+    print: main,
+  };
+}
+
+export const printers: { [astFormat: string]: Printer } = {
+  'babel-ast': createPrinter('babel'),
+  'typescript-ast': createPrinter('typescript'),
+};
