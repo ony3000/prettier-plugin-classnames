4c566bc0ae0a035a4e938f83fc419389c66b29e3
Hyeonjong <nianelo4@gmail.com>
2023-08-19 13:24:08 +0900

    test: update 'conditional' tests

--- tests/babel/conditional.test.ts
+++ tests/babel/conditional.test.ts
@@ -8,7 +8,7 @@ const options = {
 
 const fixtures: Fixture[] = [
   {
-    name: 'conditional class name #1',
+    name: 'single object, single property',
     input: `
 export function Callout({ children }) {
   return (
@@ -26,7 +26,8 @@ export function Callout({ children }) {
   return (
     <div
       className={classNames({
-        "rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50": true,
+        [\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+        dark:border-neutral-500/30 dark:bg-neutral-900/50\`]: true,
       })}
     >
       {children}
@@ -36,7 +37,7 @@ export function Callout({ children }) {
 `,
   },
   {
-    name: 'conditional class name #2 (multiple object)',
+    name: 'multiple object, single property for each object',
     input: `
 export function Callout({ children }) {
   return (
@@ -56,10 +57,12 @@ export function Callout({ children }) {
     <div
       className={classNames(
         {
-          "rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50": true,
+          [\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+          dark:border-neutral-500/30 dark:bg-neutral-900/50\`]: true,
         },
         {
-          "rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50": false,
+          [\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+          dark:border-neutral-500/30 dark:bg-neutral-900/50\`]: false,
         },
       )}
     >
@@ -70,7 +73,7 @@ export function Callout({ children }) {
 `,
   },
   {
-    name: 'conditional class name #3 (multiple property)',
+    name: 'single object, multiple property',
     input: `
 export function Callout({ children }) {
   return (
@@ -91,8 +94,10 @@ export function Callout({ children }) {
   return (
     <div
       className={classNames({
-        "bg-gray-100/50 border border-zinc-400/30 dark:bg-neutral-900/50 dark:border-neutral-500/30 px-4 py-4 rounded-xl": true,
-        "rounded-xl py-4 px-4 dark:border-neutral-500/30 dark:bg-neutral-900/50 border-zinc-400/30 border bg-gray-100/50": false,
+        [\`bg-gray-100/50 border border-zinc-400/30 dark:bg-neutral-900/50
+        dark:border-neutral-500/30 px-4 py-4 rounded-xl\`]: true,
+        [\`rounded-xl py-4 px-4 dark:border-neutral-500/30 dark:bg-neutral-900/50
+        border-zinc-400/30 border bg-gray-100/50\`]: false,
       })}
     >
       {children}
