6bd3ab252bf0c893e7daf015a41832ff06a40c76
Hyeonjong <nianelo4@gmail.com>
2023-08-20 16:22:26 +0900

    test: add option tests

---
+++ tests/babel/custom-functions.test.ts
@@ -0,0 +1,42 @@
+import type { Fixture } from '../settings';
+import { format, baseOptions } from '../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'babel',
+  customFunctions: ['clsx'],
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'custom function #1',
+    input: `
+export function Callout({ children }) {
+  const combination = clsx('rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50')
+
+  return (
+    <div className={combination}>
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  const combination = clsx(
+    \`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+    dark:border-neutral-500/30 dark:bg-neutral-900/50\`,
+  );
+
+  return <div className={combination}>{children}</div>;
+}
+`,
+  },
+];
+
+describe('babel/custom-functions', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
---
+++ tests/typescript/custom-attributes.test.ts
@@ -0,0 +1,42 @@
+import type { Fixture } from '../settings';
+import { format, baseOptions } from '../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'typescript',
+  customAttributes: ['classes'],
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'custom attribute #1',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div classes="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50">
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      classes="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+        dark:border-neutral-500/30 dark:bg-neutral-900/50"
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+];
+
+describe('typescript/custom-attributes', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
