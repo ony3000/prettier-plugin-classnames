8c32638349faee679427da0223b3d4e7bfdfc5b0
Hyeonjong <nianelo4@gmail.com>
2023-08-20 16:13:11 +0900

    feat: define plugin options

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import type { Plugin } from 'prettier';
 
+import { options } from './options';
 import { parsers } from './parsers';
 import { printers } from './printers';
 
 const classnamesPlugin: Plugin = {
   parsers,
   printers,
+  options,
 };
 
 export default classnamesPlugin;
