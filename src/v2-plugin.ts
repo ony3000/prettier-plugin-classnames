e17ee74cefee46abbd9c0fbb2823a84eec7bf921
Hyeonjong <nianelo4@gmail.com>
2023-09-23 21:10:55 +0900

    feat: uses named export instead of default export

--- src/v2-plugin.ts
+++ src/v2-plugin.ts
@@ -1,13 +1,3 @@
-import type { Plugin } from 'prettier';
-
-import { options } from './v2-parts/options';
-import { parsers } from './v2-parts/parsers';
-import { printers } from './v2-parts/printers';
-
-const classnamesPlugin: Plugin = {
-  parsers,
-  printers,
-  options,
-};
-
-export default classnamesPlugin;
+export { options } from './v2-parts/options';
+export { parsers } from './v2-parts/parsers';
+export { printers } from './v2-parts/printers';
--- src/v3-plugin.ts
+++ src/v3-plugin.ts
@@ -1,13 +1,3 @@
-import type { Plugin } from 'prettier3';
-
-import { options } from './v3-parts/options';
-import { parsers } from './v3-parts/parsers';
-import { printers } from './v3-parts/printers';
-
-const classnamesPlugin: Plugin = {
-  parsers,
-  printers,
-  options,
-};
-
-export default classnamesPlugin;
+export { options } from './v3-parts/options';
+export { parsers } from './v3-parts/parsers';
+export { printers } from './v3-parts/printers';
