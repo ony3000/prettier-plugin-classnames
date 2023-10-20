032772e7191f7a40cb8f8decb556bc45d36228d7
Hyeonjong <nianelo4@gmail.com>
2023-10-20 23:40:13 +0900

    chore: remove things related to src directory

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
+  moduleNameMapper: {},
   testEnvironment: 'node',
 };
