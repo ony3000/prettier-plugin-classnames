2bca1b6c459cb028895edf163c0039c26802367d
Hyeonjong <nianelo4@gmail.com>
2024-01-14 20:13:00 +0900

    style: apply format to all workspaces

--- tests/babel/custom-functions.test.ts
+++ tests/babel/custom-functions.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from 'vitest';
+
 import type { Fixture } from '../settings';
 import { format, baseOptions } from '../settings';
 
--- tests/vue/others.test.ts
+++ tests/vue/others.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from 'vitest';
+
 import { format, baseOptions } from '../settings';
 
 const options = {
