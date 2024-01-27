e72c1ce56b2319901bfe20f1bc38f91b1876fb8a
Hyeonjong <nianelo4@gmail.com>
2024-01-27 21:52:12 +0900

    feat: add some kind of 'position' option

--- src/packages/v2-plugin/options.ts
+++ src/packages/v2-plugin/options.ts
@@ -19,4 +19,21 @@ export const options: SupportOptions = {
     description:
       'List of functions that enclosing class names. The `classNames` function is always supported, even if no options are specified.',
   },
+  endingPosition: {
+    since: '0.5.0',
+    type: 'choice',
+    category: 'Format',
+    default: 'relative',
+    description: 'This is the criterion for ending the class name on each line when replacing the original class name with a multi-line class name.',
+    choices: [
+      {
+        value: 'relative',
+        description: 'Each line ends at a `printWidth` distance from the start of the class name.',
+      },
+      {
+        value: 'absolute',
+        description: 'Each line ends at a `printWidth` distance from the start of the line.',
+      },
+    ]
+  },
 };
