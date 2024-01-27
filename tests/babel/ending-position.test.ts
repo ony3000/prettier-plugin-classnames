e02ae56ef1a35a9accae3bf4d9b8ca3c24aa34dd
Hyeonjong <nianelo4@gmail.com>
2024-01-27 22:30:03 +0900

    test: add absolute position tests

---
+++ tests/babel/ending-position.test.ts
@@ -0,0 +1,154 @@
+import { describe, expect, test } from 'vitest';
+
+import type { Fixture } from '../settings';
+import { format, baseOptions } from '../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'babel',
+  endingPosition: 'absolute',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'enclosed-in-quotes #1',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50">
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4
+py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50"
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'embedded-expression #1',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className={'rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50'}>
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className={\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+dark:border-neutral-500/30 dark:bg-neutral-900/50\`}
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'embedded-expression #2',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div className={classNames('rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50')}>
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className={classNames(
+        \`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+dark:border-neutral-500/30 dark:bg-neutral-900/50\`,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'conditional #1',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div
+      className={classNames(
+        { 'rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50': true },
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className={classNames({
+        [\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+dark:border-neutral-500/30 dark:bg-neutral-900/50\`]: true,
+      })}
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'prettier-ignore #1',
+    input: `
+export function Callout({ children }) {
+  return (
+    <div
+      className={classNames(
+        // prettier-ignore
+        'bg-gray-100/50 border border-zinc-400/30 dark:bg-neutral-900/50 dark:border-neutral-500/30 px-4 py-4 rounded-xl',
+        'rounded-xl py-4 px-4 dark:border-neutral-500/30 dark:bg-neutral-900/50 border-zinc-400/30 border bg-gray-100/50',
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+`,
+    output: `export function Callout({ children }) {
+  return (
+    <div
+      className={classNames(
+        // prettier-ignore
+        'bg-gray-100/50 border border-zinc-400/30 dark:bg-neutral-900/50 dark:border-neutral-500/30 px-4 py-4 rounded-xl',
+        \`rounded-xl py-4 px-4 dark:border-neutral-500/30 dark:bg-neutral-900/50
+border-zinc-400/30 border bg-gray-100/50\`,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+`,
+  },
+];
+
+describe('babel/ending-position', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
---
+++ tests/vue/ending-position.test.ts
@@ -0,0 +1,130 @@
+import { describe, expect, test } from 'vitest';
+
+import type { Fixture } from '../settings';
+import { format, baseOptions } from '../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'vue',
+  endingPosition: 'absolute',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'enclosed-in-quotes #1',
+    input: `
+<template>
+  <div class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50">
+    <slot></slot>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div
+    class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+dark:border-neutral-500/30 dark:bg-neutral-900/50"
+  >
+    <slot></slot>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'attribute-bindings #1',
+    input: `
+<template>
+  <div :class="'rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50'">
+    <slot></slot>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div
+    :class="\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+dark:border-neutral-500/30 dark:bg-neutral-900/50\`"
+  >
+    <slot></slot>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'attribute-bindings #2',
+    input: `
+<template>
+  <div :class="classNames('rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50')">
+    <slot></slot>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div
+    :class="
+      classNames(
+        \`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+dark:border-neutral-500/30 dark:bg-neutral-900/50\`,
+      )
+    "
+  >
+    <slot></slot>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'conditional #1',
+    input: `
+<template>
+  <div :class="{ 'rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50': true }">
+    <slot></slot>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div
+    :class="{
+      [\`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+dark:border-neutral-500/30 dark:bg-neutral-900/50\`]: true,
+    }"
+  >
+    <slot></slot>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'prettier-ignore #1',
+    input: `
+<template>
+  <div :class="[
+    // prettier-ignore
+    'bg-gray-100/50 border border-zinc-400/30 dark:bg-neutral-900/50 dark:border-neutral-500/30 px-4 py-4 rounded-xl',
+    'rounded-xl py-4 px-4 dark:border-neutral-500/30 dark:bg-neutral-900/50 border-zinc-400/30 border bg-gray-100/50',
+  ]">
+    <slot></slot>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div
+    :class="[
+      // prettier-ignore
+      'bg-gray-100/50 border border-zinc-400/30 dark:bg-neutral-900/50 dark:border-neutral-500/30 px-4 py-4 rounded-xl',
+      \`rounded-xl py-4 px-4 dark:border-neutral-500/30 dark:bg-neutral-900/50
+border-zinc-400/30 border bg-gray-100/50\`,
+    ]"
+  >
+    <slot></slot>
+  </div>
+</template>
+`,
+  },
+];
+
+describe('vue/ending-position', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
