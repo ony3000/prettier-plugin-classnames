80fc9b9a377248fb29572a0e289aa1c23e3b3b46
Hyeonjong <nianelo4@gmail.com>
2024-01-31 22:10:11 +0900

    test: fix typo

--- tests/babel/reversibility.test.ts
+++ tests/babel/reversibility.test.ts
@@ -271,7 +271,7 @@ export function Callout({ children }) {
   return (
     <div
       className={
-        \`${""}\` +
+        \`\${""}\` +
         "rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4"
       }
     >
