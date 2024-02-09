eb51c92c5d2a78742d5d802ee0b9aab6c879cbe4
Hyeonjong <nianelo4@gmail.com>
2024-02-09 15:35:23 +0900

    test: add astro parser tests

---
+++ tests/v2-test/astro/attribute-without-expression.test.ts
@@ -0,0 +1,153 @@
+import { format } from 'prettier';
+import type { Fixture } from 'test-settings';
+import { baseOptions } from 'test-settings';
+import { describe, expect, test } from 'vitest';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as thisPlugin from '@/packages/v2-plugin';
+
+const options = {
+  ...baseOptions,
+  plugins: ['prettier-plugin-astro', thisPlugin],
+  parser: 'astro',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'class attribute only (1)',
+    input: `
+<div class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50">
+  <slot />
+</div>
+`,
+    output: `<div
+  class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+    dark:border-neutral-500/30 dark:bg-neutral-900/50"
+>
+  <slot />
+</div>
+`,
+  },
+  {
+    name: 'class attribute only (2) - short enough class name',
+    input: `
+<div class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4">
+  <slot />
+</div>
+`,
+    output: `<div class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4">
+  <slot />
+</div>
+`,
+  },
+  {
+    name: 'along with other attributes (1) - class is the first',
+    input: `
+<div
+  class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50"
+  tabindex="-1"
+  title="Callout"
+>
+  <slot />
+</div>
+`,
+    output: `<div
+  class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+    dark:border-neutral-500/30 dark:bg-neutral-900/50"
+  tabindex="-1"
+  title="Callout"
+>
+  <slot />
+</div>
+`,
+  },
+  {
+    name: 'along with other attributes (2) - class is the last',
+    input: `
+<div
+  tabindex="-1"
+  title="Callout"
+  class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50"
+>
+  <slot />
+</div>
+`,
+    output: `<div
+  tabindex="-1"
+  title="Callout"
+  class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+    dark:border-neutral-500/30 dark:bg-neutral-900/50"
+>
+  <slot />
+</div>
+`,
+  },
+  {
+    name: 'custom attributes',
+    input: `
+<div>
+  <Callout classes="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50">
+    Lorem ipsum
+  </Callout>
+</div>
+`,
+    output: `<div>
+  <Callout
+    classes="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+      dark:border-neutral-500/30 dark:bg-neutral-900/50"
+  >
+    Lorem ipsum
+  </Callout>
+</div>
+`,
+    options: {
+      customAttributes: ['classes'],
+    },
+  },
+  {
+    name: 'reversibility - short enough multi-line class name',
+    input: `
+<div
+  class="rounded-xl border border-zinc-400/30
+    bg-gray-100/50 px-4 py-4"
+>
+  <slot />
+</div>
+`,
+    output: `<div class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4">
+  <slot />
+</div>
+`,
+  },
+  {
+    name: 'ending position',
+    input: `
+<div class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50">
+  <slot />
+</div>
+`,
+    output: `<div
+  class="rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+dark:border-neutral-500/30 dark:bg-neutral-900/50"
+>
+  <slot />
+</div>
+`,
+    options: {
+      endingPosition: 'absolute',
+    },
+  },
+];
+
+describe('astro/attribute-without-expression', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(
+        format(fixture.input, {
+          ...options,
+          ...(fixture.options ?? {}),
+        }),
+      ).toBe(fixture.output);
+    });
+  }
+});
---
+++ tests/v3-test/astro/variable-declaration.test.ts
@@ -0,0 +1,61 @@
+import { format } from 'prettier';
+import type { Fixture } from 'test-settings';
+import { baseOptions } from 'test-settings';
+import { describe, expect, test } from 'vitest';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as thisPlugin from '@/packages/v3-plugin';
+
+const options = {
+  ...baseOptions,
+  plugins: ['prettier-plugin-astro', thisPlugin],
+  parser: 'astro',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'string literal (1)',
+    input: `
+---
+const combination = 'rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50'
+---
+`,
+    output: `---
+const combination =
+  "rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50";
+---
+`,
+  },
+  {
+    name: 'string literal (2) - wrapped in `classNames`',
+    input: `
+---
+import classNames from 'classnames'
+
+const combination = classNames('rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4 dark:border-neutral-500/30 dark:bg-neutral-900/50')
+---
+`,
+    output: `---
+import classNames from "classnames";
+
+const combination = classNames(
+  \`rounded-xl border border-zinc-400/30 bg-gray-100/50 px-4 py-4
+  dark:border-neutral-500/30 dark:bg-neutral-900/50\`,
+);
+---
+`,
+  },
+];
+
+describe('astro/variable-declaration', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, async () => {
+      expect(
+        await format(fixture.input, {
+          ...options,
+          ...(fixture.options ?? {}),
+        }),
+      ).toBe(fixture.output);
+    });
+  }
+});
