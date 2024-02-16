ae9053f3534b3a85ee44645c8e97c8a8914f7d9d
Hyeonjong <nianelo4@gmail.com>
2024-02-16 23:18:49 +0900

    test: add tests that reproduce #37

--- tests/v3-test/vue/expression.test.ts
+++ tests/v3-test/vue/expression.test.ts
@@ -385,6 +385,27 @@ import clsx from "clsx";
 `,
   },
   {
+    name: 'issue #37 - short enough conditional class name (no error in v0.4.0, error in v0.5.0 ~ v0.6.0)',
+    input: `
+<template>
+  <div
+    :class="{
+        'bg-black': true
+    }">Some text</div>
+</template>
+`,
+    output: `<template>
+  <div
+    :class="{
+      'bg-black': true,
+    }"
+  >
+    Some text
+  </div>
+</template>
+`,
+  },
+  {
     name: 'ending position (1)',
     input: `
 <template>
