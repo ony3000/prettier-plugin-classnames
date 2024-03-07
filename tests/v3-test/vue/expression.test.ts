8c4c1fcc37a221cdb794daa09a43efaaee219872
Hyeonjong <nianelo4@gmail.com>
2024-03-07 22:19:40 +0900

    test: fix output of double nested expression tests
    The difference occurred because the prettier version depended on by this plugin and the prettier version of the prettier playground were different.

--- tests/v3-test/vue/expression.test.ts
+++ tests/v3-test/vue/expression.test.ts
@@ -853,7 +853,9 @@ dark:border-neutral-500/30 dark:bg-neutral-900/50\`]: true,
       ({ active }) =>
         \`relative cursor-default select-none py-2 pl-10 pr-4 \${
           active
-            ? \`bg-teal-600 \${active ? 'bg-teal-600 text-white' : 'text-gray-900'} text-white\`
+            ? \`bg-teal-600 \${
+                active ? 'bg-teal-600 text-white' : 'text-gray-900'
+              } text-white\`
             : 'text-gray-900'
         }\`
     "
