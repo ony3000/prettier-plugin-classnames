fd72bae3031867f6a9a7b7112229400a40e9a9e2
Hyeonjong <nianelo4@gmail.com>
2024-03-23 20:53:17 +0900

    test: add tests for #37

---
+++ tests/v2-test/vue/issue-37/relative.test.ts
@@ -0,0 +1,56 @@
+import { format } from 'prettier';
+import type { Fixture } from 'test-settings';
+import { baseOptions } from 'test-settings';
+import { describe, expect, test } from 'vitest';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as thisPlugin from '@/packages/v2-plugin';
+
+const options = {
+  ...baseOptions,
+  plugins: [thisPlugin],
+  parser: 'vue',
+  endingPosition: 'relative',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'short enough object key (no error in v0.4.0, error in v0.5.0 ~ v0.6.0)',
+    input: `
+<template>
+  <div
+    :class="{
+        'bg-black': true
+    }">Some text</div>
+</template>
+`,
+    output: `<template>
+  <div
+    :class="{
+      'bg-black': true,
+    }"
+  >
+    Some text
+  </div>
+</template>
+`,
+  },
+];
+
+describe.each(fixtures)('$name', ({ input, output, options: fixtureOptions }) => {
+  const fixedOptions = {
+    ...options,
+    ...(fixtureOptions ?? {}),
+  };
+  const formattedText = format(input, fixedOptions);
+
+  test('expectation', () => {
+    expect(formattedText).toBe(output);
+  });
+
+  test.runIf(formattedText === output)('consistency', () => {
+    const doubleFormattedText = format(formattedText, fixedOptions);
+
+    expect(doubleFormattedText).toBe(formattedText);
+  });
+});
