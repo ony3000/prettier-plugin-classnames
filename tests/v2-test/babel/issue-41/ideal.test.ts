d35343813a547b0f7aa205376e62045c30684de7
Hyeonjong <nianelo4@gmail.com>
2024-03-23 10:12:40 +0900

    test: add tests for #41

---
+++ tests/v2-test/babel/issue-41/ideal.test.ts
@@ -0,0 +1,82 @@
+import { format } from 'prettier';
+import type { Fixture } from 'test-settings';
+import { baseOptions } from 'test-settings';
+import { describe, expect, test } from 'vitest';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as thisPlugin from '@/packages/v2-plugin';
+
+const options = {
+  ...baseOptions,
+  plugins: [thisPlugin],
+  parser: 'babel',
+  endingPosition: 'absolute-with-indent',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'one input',
+    input: `
+export default function MyComponent() {
+  return (
+    <section className="lorem ipsum dolor sit amet consectetur adipiscing elit aenean p">
+      content
+    </section>
+  );
+}
+`,
+    output: `export default function MyComponent() {
+  return (
+    <section
+      className="lorem ipsum dolor sit amet consectetur adipiscing elit aenean p"
+    >
+      content
+    </section>
+  );
+}
+`,
+  },
+  {
+    name: 'another input',
+    input: `
+export default function MyComponent() {
+  return (
+    <section
+      className="lorem ipsum dolor sit amet consectetur adipiscing elit
+        aenean p"
+    >
+      content
+    </section>
+  );
+}
+`,
+    output: `export default function MyComponent() {
+  return (
+    <section
+      className="lorem ipsum dolor sit amet consectetur adipiscing elit aenean p"
+    >
+      content
+    </section>
+  );
+}
+`,
+  },
+];
+
+describe.each(fixtures)('$name', ({ input, output, options: fixtureOptions }) => {
+  const fixedOptions = {
+    ...options,
+    ...(fixtureOptions ?? {}),
+  };
+  const formattedText = format(input, fixedOptions);
+
+  test('expectation', () => {
+    expect(formattedText).toBe(output);
+  });
+
+  test.runIf(formattedText === output)('consistency', () => {
+    const doubleFormattedText = format(formattedText, fixedOptions);
+
+    expect(doubleFormattedText).toBe(formattedText);
+  });
+});
---
+++ tests/v3-test/babel/issue-41/ideal.test.ts
@@ -0,0 +1,82 @@
+import { format } from 'prettier';
+import type { Fixture } from 'test-settings';
+import { baseOptions } from 'test-settings';
+import { describe, expect, test } from 'vitest';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as thisPlugin from '@/packages/v3-plugin';
+
+const options = {
+  ...baseOptions,
+  plugins: [thisPlugin],
+  parser: 'babel',
+  endingPosition: 'absolute-with-indent',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'one input',
+    input: `
+export default function MyComponent() {
+  return (
+    <section className="lorem ipsum dolor sit amet consectetur adipiscing elit aenean p">
+      content
+    </section>
+  );
+}
+`,
+    output: `export default function MyComponent() {
+  return (
+    <section
+      className="lorem ipsum dolor sit amet consectetur adipiscing elit aenean p"
+    >
+      content
+    </section>
+  );
+}
+`,
+  },
+  {
+    name: 'another input',
+    input: `
+export default function MyComponent() {
+  return (
+    <section
+      className="lorem ipsum dolor sit amet consectetur adipiscing elit
+        aenean p"
+    >
+      content
+    </section>
+  );
+}
+`,
+    output: `export default function MyComponent() {
+  return (
+    <section
+      className="lorem ipsum dolor sit amet consectetur adipiscing elit aenean p"
+    >
+      content
+    </section>
+  );
+}
+`,
+  },
+];
+
+describe.each(fixtures)('$name', async ({ input, output, options: fixtureOptions }) => {
+  const fixedOptions = {
+    ...options,
+    ...(fixtureOptions ?? {}),
+  };
+  const formattedText = await format(input, fixedOptions);
+
+  test('expectation', () => {
+    expect(formattedText).toBe(output);
+  });
+
+  test.runIf(formattedText === output)('consistency', async () => {
+    const doubleFormattedText = await format(formattedText, fixedOptions);
+
+    expect(doubleFormattedText).toBe(formattedText);
+  });
+});
