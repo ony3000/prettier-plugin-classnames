da299688ca705341739f23b2a9bfbd5a94c8cbb7
Hyeonjong <nianelo4@gmail.com>
2024-06-03 23:37:20 +0900

    test: add html parser tests
    Since this is simply a copy of the vue test, it can be modified during the feature implementation process.

---
+++ tests/v2-test/html/string-literal-ternary/ideal.test.ts
@@ -0,0 +1,349 @@
+import type { Fixture } from 'test-settings';
+import { baseOptions } from 'test-settings';
+
+import { thisPlugin, testEach } from '../../adaptor';
+
+const options = {
+  ...baseOptions,
+  plugins: [thisPlugin],
+  parser: 'html',
+  printWidth: 60,
+  endingPosition: 'absolute-with-indent',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'short enough (1) - single line with no spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="condition ? 'lorem ipsum dolor sit amet' : 'lorem ipsum dolor sit amet'">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? 'lorem ipsum dolor sit amet'
+          : 'lorem ipsum dolor sit amet'
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'short enough (2) - single line with spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="condition ? '  lorem ipsum dolor sit amet  ' : '  lorem ipsum dolor sit amet  '">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? ' lorem ipsum dolor sit amet '
+          : ' lorem ipsum dolor sit amet '
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'short enough (3) - multiple lines',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="
+      condition ? 'lorem ipsum\\
+      dolor sit amet' : 'lorem ipsum\\
+      dolor sit amet'
+    ">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? 'lorem ipsum dolor sit amet'
+          : 'lorem ipsum dolor sit amet'
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'near boundary (1) - single line with no spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="condition ? 'lorem ipsum dolor sit amet consectetur adipiscing elit proin' : 'lorem ipsum dolor sit amet consectetur adipiscing elit proin'">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin\`
+          : \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin\`
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'near boundary (2) - single line with spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="condition ? '   lorem ipsum dolor sit amet consectetur adipiscing elit proin   ' : '   lorem ipsum dolor sit amet consectetur adipiscing elit proin   '">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? \` lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin \`
+          : \` lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin \`
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'near boundary (3) - multiple lines',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="
+      condition ? 'lorem ipsum\\
+      dolor sit amet\\
+      consectetur adipiscing elit\\
+      proin' : 'lorem ipsum\\
+      dolor sit amet\\
+      consectetur adipiscing elit\\
+      proin'
+    ">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin\`
+          : \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin\`
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'long enough (1) - single line with no spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="condition ? 'lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque' : 'lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque'">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere eu volutpat id neque pellentesque\`
+          : \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere eu volutpat id neque pellentesque\`
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'long enough (2) - single line with spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="condition ? '    lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque    ' : '    lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque    '">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? \` lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere eu volutpat id neque pellentesque \`
+          : \` lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere eu volutpat id neque pellentesque \`
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'long enough (3) - multiple lines',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="
+      condition ? 'lorem ipsum\\
+      dolor sit amet\\
+      consectetur adipiscing elit\\
+      proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque' : 'lorem ipsum\\
+      dolor sit amet\\
+      consectetur adipiscing elit\\
+      proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque'
+    ">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        condition
+          ? \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere eu volutpat id neque pellentesque\`
+          : \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere eu volutpat id neque pellentesque\`
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'syntax variants (1) - component',
+    input: `
+<template>
+  <div>
+    <Box v-bind:class="condition ? 'lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere' : 'lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere'">
+      <slot></slot>
+    </Box>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <Box
+      v-bind:class="
+        condition
+          ? \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere\`
+          : \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere\`
+      "
+    >
+      <slot></slot>
+    </Box>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'syntax variants (2) - shorthand',
+    input: `
+<template>
+  <div>
+    <div :class="condition ? 'lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere' : 'lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere'">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      :class="
+        condition
+          ? \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere\`
+          : \`lorem ipsum dolor sit amet consectetur
+            adipiscing elit proin ex massa hendrerit eu
+            posuere\`
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+];
+
+testEach(fixtures, options);
---
+++ tests/v3-test/html/string-literal-argument/absolute.test.ts
@@ -0,0 +1,343 @@
+import type { Fixture } from 'test-settings';
+import { baseOptions } from 'test-settings';
+
+import { thisPlugin, testEach } from '../../adaptor';
+
+const options = {
+  ...baseOptions,
+  plugins: [thisPlugin],
+  parser: 'html',
+  printWidth: 60,
+  endingPosition: 'absolute',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'short enough (1) - single line with no spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames('lorem ipsum dolor sit amet')">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames('lorem ipsum dolor sit amet')
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'short enough (2) - single line with spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames('  lorem ipsum dolor sit amet  ')">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames(' lorem ipsum dolor sit amet ')
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'short enough (3) - multiple lines',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames(
+      'lorem ipsum\\
+      dolor sit amet'
+    )">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames('lorem ipsum dolor sit amet')
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'near boundary (1) - single line with no spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames('lorem ipsum dolor sit amet consectetur adipiscing elit proin')">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames(
+          \`lorem ipsum dolor sit amet consectetur adipiscing
+elit proin\`,
+        )
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'near boundary (2) - single line with spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames('   lorem ipsum dolor sit amet consectetur adipiscing elit proin   ')">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames(
+          \` lorem ipsum dolor sit amet consectetur
+adipiscing elit proin \`,
+        )
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'near boundary (3) - multiple lines',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames(
+      'lorem ipsum\\
+      dolor sit amet\\
+      consectetur adipiscing elit\\
+      proin'
+    )">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames(
+          \`lorem ipsum dolor sit amet consectetur adipiscing
+elit proin\`,
+        )
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'long enough (1) - single line with no spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames('lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque')">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames(
+          \`lorem ipsum dolor sit amet consectetur adipiscing
+elit proin ex massa hendrerit eu posuere eu volutpat id
+neque pellentesque\`,
+        )
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'long enough (2) - single line with spaces at both ends',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames('    lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque    ')">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames(
+          \` lorem ipsum dolor sit amet consectetur
+adipiscing elit proin ex massa hendrerit eu posuere eu
+volutpat id neque pellentesque \`,
+        )
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'long enough (3) - multiple lines',
+    input: `
+<template>
+  <div>
+    <div v-bind:class="classNames(
+      'lorem ipsum\\
+      dolor sit amet\\
+      consectetur adipiscing elit\\
+      proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque'
+    )">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      v-bind:class="
+        classNames(
+          \`lorem ipsum dolor sit amet consectetur adipiscing
+elit proin ex massa hendrerit eu posuere eu volutpat id
+neque pellentesque\`,
+        )
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'syntax variants (1) - component',
+    input: `
+<template>
+  <div>
+    <Box v-bind:class="classNames('lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere')">
+      <slot></slot>
+    </Box>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <Box
+      v-bind:class="
+        classNames(
+          \`lorem ipsum dolor sit amet consectetur adipiscing
+elit proin ex massa hendrerit eu posuere\`,
+        )
+      "
+    >
+      <slot></slot>
+    </Box>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'syntax variants (2) - shorthand',
+    input: `
+<template>
+  <div>
+    <div :class="classNames('lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere')">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+    output: `<template>
+  <div>
+    <div
+      :class="
+        classNames(
+          \`lorem ipsum dolor sit amet consectetur adipiscing
+elit proin ex massa hendrerit eu posuere\`,
+        )
+      "
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+`,
+  },
+  {
+    name: 'syntax variants (3) - script tag',
+    input: `
+<script setup lang="ts">
+const combination = classNames([
+  'lorem ipsum dolor sit amet',
+  {'lorem ipsum dolor sit amet': true},
+  condition ? 'lorem ipsum dolor sit amet' : 'lorem ipsum dolor sit amet',
+])
+</script>
+`,
+    output: `<script setup lang="ts">
+const combination = classNames([
+  "lorem ipsum dolor sit amet",
+  { "lorem ipsum dolor sit amet": true },
+  condition
+    ? "lorem ipsum dolor sit amet"
+    : "lorem ipsum dolor sit amet",
+]);
+</script>
+`,
+  },
+];
+
+testEach(fixtures, options);
