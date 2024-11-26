562b337b6652d98e8115583826b3057f849b5bb7
Hyeonjong <nianelo4@gmail.com>
2024-11-26 22:29:07 +0900

    test: add tests that reproduce #72

---
+++ tests/v2-test/typescript/issue-72/fixtures.ts
@@ -0,0 +1,38 @@
+import type { Fixture } from 'test-settings';
+
+export const fixtures: Omit<Fixture, 'output'>[] = [
+  {
+    name: '(1) If the end position is absolute, the delimiter following the class name is also included in the line wrapping.',
+    input: `
+//------------------------------------------------------------------| printWidth=69 (in snapshot)
+export function Foo({ children }) {
+  return (
+    <div className="lorem ipsum dolor sit amet consectetur adipiscing">
+      {children}
+    </div>
+  );
+}
+`,
+    options: {
+      printWidth: 69,
+      experimentalOptimization: true,
+    },
+  },
+  {
+    name: '(2) If the end position is absolute, the delimiter following the class name is also included in the line wrapping.',
+    input: `
+//-------------------------------------------------------| printWidth=58 (in snapshot)
+export function Foo({ children }) {
+  return (
+    <div className={'lorem ipsum dolor sit amet consectetur adipiscing'}>
+      {children}
+    </div>
+  );
+}
+`,
+    options: {
+      printWidth: 58,
+      experimentalOptimization: true,
+    },
+  },
+];
