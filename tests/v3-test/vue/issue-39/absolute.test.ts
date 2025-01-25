b09a96f7a0d52a74e723090cbb4d3824168b4313
Hyeonjong <nianelo4@gmail.com>
2025-01-25 21:29:16 +0900

    test: split another nested expression test

--- tests/v3-test/vue/issue-39/absolute.test.ts
+++ tests/v3-test/vue/issue-39/absolute.test.ts
@@ -38,6 +38,9 @@ active ? 'bg-teal-600 text-white' : 'text-gray-900' }\`
   ></Combobox.Option>
 </template>
 `,
+    options: {
+      experimentalOptimization: false,
+    },
   },
   {
     name: 'double nested expression in template literal',
@@ -70,6 +73,77 @@ active ? 'bg-teal-600 text-white' : 'text-gray-900' } text-white\`
   ></Combobox.Option>
 </template>
 `,
+    options: {
+      experimentalOptimization: false,
+    },
+  },
+  {
+    name: '(exp-1) nested expression in template literal',
+    input: `
+<template>
+    <Combobox.Option
+        :class="({ active }) =>
+            \`relative cursor-default select-none py-2 pl-10 pr-4 \${
+                active
+                    ? 'bg-teal-600 text-white'
+                    : 'text-gray-900'
+            }\`
+        "
+        :value="'test'"
+    ></Combobox.Option>
+</template>
+`,
+    output: `<template>
+  <Combobox.Option
+    :class="
+      ({ active }) =>
+        \`relative cursor-default select-none py-2 pl-10 pr-4 \${
+          active ? 'bg-teal-600 text-white' : 'text-gray-900'
+}\`
+    "
+    :value="'test'"
+  ></Combobox.Option>
+</template>
+`,
+    options: {
+      experimentalOptimization: true,
+    },
+  },
+  {
+    name: '(exp-2) double nested expression in template literal',
+    input: `
+<template>
+    <Combobox.Option
+        :class="({ active }) =>
+            \`relative cursor-default select-none py-2 pl-10 pr-4 \${
+                active
+                    ? \`bg-teal-600 \${active ? 'bg-teal-600 text-white' : 'text-gray-900'} text-white\`
+                    : 'text-gray-900'
+            }\`
+        "
+        :value="'test'"
+    ></Combobox.Option>
+</template>
+`,
+    output: `<template>
+  <Combobox.Option
+    :class="
+      ({ active }) =>
+        \`relative cursor-default select-none py-2 pl-10 pr-4 \${
+          active
+            ? \`bg-teal-600 \${
+                active ? 'bg-teal-600 text-white' : 'text-gray-900'
+} text-white\`
+            : 'text-gray-900'
+}\`
+    "
+    :value="'test'"
+  ></Combobox.Option>
+</template>
+`,
+    options: {
+      experimentalOptimization: true,
+    },
   },
 ];
 
