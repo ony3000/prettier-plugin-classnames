681423ebe2efbd65abd46115c09f27c5191a0ad1
Hyeonjong <nianelo4@gmail.com>
2025-05-13 00:02:57 +0900

    chore: remove plugin for prettier v2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-// eslint-disable-next-line import/no-import-module-exports
-import prettier from 'prettier';
-
-if (prettier.version.startsWith('2.')) {
-  // eslint-disable-next-line global-require
-  module.exports = require('./packages/v2-plugin');
-} else {
-  // eslint-disable-next-line global-require
-  module.exports = require('./packages/v3-plugin');
-}
+// eslint-disable-next-line global-require
+module.exports = require('./packages/v3-plugin');
