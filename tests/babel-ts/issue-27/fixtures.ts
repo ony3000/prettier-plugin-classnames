c7d7b41a28623e1e14397e24b3bc01d51b65efc2
Hyeonjong <nianelo4@gmail.com>
2026-01-11 18:39:40 +0900

    test: copy typescript tests to babel-ts tests

---
+++ tests/babel-ts/issue-27/fixtures.ts
@@ -0,0 +1,64 @@
+import type { Fixture } from '../../settings';
+
+export const fixtures: Omit<Fixture, 'output'>[] = [
+  {
+    name: 'template literal ending with a space',
+    input: `
+//-----------------------------------------------------------------------------| printWidth=80 (in snapshot)
+export default function Test() {
+  return (
+    <div>
+      {[0, 1, 2, 3].map((key) => {
+        return (
+          <div
+            key={key}
+            className={\`\${key ? \`relative \` : \`\`}
+            tablet:static\`}>{key}</div>
+        );
+      })}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'template literal starting with a space',
+    input: `
+//-----------------------------------------------------------------------------| printWidth=80 (in snapshot)
+export default function Test() {
+  return (
+    <div>
+      {[0, 1, 2, 3].map((key) => {
+        return (
+          <div
+            key={key}
+            className={\`\${key ? \` relative\` : \`\`}
+            tablet:static\`}>{key}</div>
+        );
+      })}
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'trimmed template literal (no error in v0.4.0, error in v0.5.0)',
+    input: `
+//-----------------------------------------------------------------------------| printWidth=80 (in snapshot)
+export default function Test() {
+  return (
+    <div>
+      {[0, 1, 2, 3].map((key) => {
+        return (
+          <div
+            key={key}
+            className={\`\${key ? \`relative\` : \`\`}
+            tablet:static\`}>{key}</div>
+        );
+      })}
+    </div>
+  );
+}
+`,
+  },
+];
