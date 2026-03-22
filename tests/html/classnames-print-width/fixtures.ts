fdbe59e15067a3a8c9c3263262261887f0622d18
Hyeonjong <nianelo4@gmail.com>
2026-03-22 13:02:46 +0900

    Add tests for supported parsers

---
+++ tests/html/classnames-print-width/fixtures.ts
@@ -0,0 +1,56 @@
+import type { Fixture } from '../../settings';
+
+export const fixtures: Omit<Fixture, 'output'>[] = [
+  {
+    name: '(1) If no value is provided, the `printWidth` value is used as default.',
+    input: `
+<!-- --------------------------------------------------------------------------| printWidth=80 (in snapshot) -->
+<div>
+  <div>
+    <div class="lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque">
+      <span></span>
+    </div>
+  </div>
+</div>
+`,
+    options: {
+      printWidth: 80,
+    },
+  },
+  {
+    name: '(2) `classnamesPrintWidth` is shorter than `printWidth`.',
+    input: `
+<!-- --------------------------------------------------------------------------| printWidth=80 (in snapshot) -->
+<!-- -------------------------------------------------------| printWidth=60 (in snapshot) -->
+<div>
+  <div>
+    <div class="lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque">
+      <span></span>
+    </div>
+  </div>
+</div>
+`,
+    options: {
+      printWidth: 80,
+      classnamesPrintWidth: 60,
+    },
+  },
+  {
+    name: '(3) `classnamesPrintWidth` is longer than `printWidth`.',
+    input: `
+<!-- --------------------------------------------------------------------------| printWidth=80 (in snapshot) -->
+<!-- -------------------------------------------------------| printWidth=60 (in snapshot) -->
+<div>
+  <div>
+    <div class="lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque">
+      <span></span>
+    </div>
+  </div>
+</div>
+`,
+    options: {
+      printWidth: 60,
+      classnamesPrintWidth: 80,
+    },
+  },
+];
