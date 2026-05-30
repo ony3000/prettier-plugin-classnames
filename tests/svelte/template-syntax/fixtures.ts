edc8dea855543953cc3b9e3379ff940993777603
Hyeonjong <nianelo4@gmail.com>
2026-05-31 00:09:25 +0900

    Add tests that fail in prettier-plugin-svelte 3.x

--- tests/svelte/template-syntax/fixtures.ts
+++ tests/svelte/template-syntax/fixtures.ts
@@ -79,4 +79,33 @@ export const fixtures: Omit<Fixture, 'output'>[] = [
       printWidth: 60,
     },
   },
+  // Note: The following tests fail in prettier-plugin-svelte 3.x.
+  //   {
+  //     name: '(5-1) #each',
+  //     input: `
+  // <!-- ------------------------------------------------------| printWidth=60 (in snapshot) -->
+  // {#each { length: 8 }, index}
+  //   <div class="lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque"></div>
+  // {/each}
+  // `,
+  //     options: {
+  //       printWidth: 60,
+  //     },
+  //   },
+  //   {
+  //     name: '(5-2) #snippet',
+  //     input: `
+  // <!-- ------------------------------------------------------| printWidth=60 (in snapshot) -->
+  // {#snippet foo()}
+  //   <div class="lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque"></div>
+  // {/snippet}
+
+  // {#snippet bar(param1, param2, paramN)}
+  //   <div class="lorem ipsum dolor sit amet consectetur adipiscing elit proin ex massa hendrerit eu posuere eu volutpat id neque pellentesque"></div>
+  // {/snippet}
+  // `,
+  //     options: {
+  //       printWidth: 60,
+  //     },
+  //   },
 ];
